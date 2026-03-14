e739275ec0e52b74970881e4481fb8046e7fe9e1
Juan David <jdavid.canoh@gmail.com>
2026-03-14 17:44:51 -0500

    add more aliases

--- config/user_aliases.cmd
+++ config/user_aliases.cmd
@@ -5,15 +5,13 @@
 ;= goto:eof
 ;= Add aliases below here
 e.=explorer .
-gl=git log --oneline --all --graph --decorate  $*
-l=ls --show-control-chars -CFGNhp --color --ignore={"NTUSER.DAT*","ntuser.dat*"} $*
-ls=ls --show-control-chars -F --color $*
 pwd=cd
 clear=cls
 unalias=alias /d $1
 vi=vim $*
 cmderr=cd /d "%CMDER_ROOT%"
-pwsh=%SystemRoot%/System32/WindowsPowerShell/v1.0/powershell.exe -ExecutionPolicy Bypass -NoLogo -NoProfile -NoExit -Command "Invoke-Expression '. ''%CMDER_ROOT%/vendor/profile.ps1'''"save_terminal=cp C:\Users\juan\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json $1 /Y  
+pwsh=%SystemRoot%/System32/WindowsPowerShell/v1.0/powershell.exe -ExecutionPolicy Bypass -NoLogo -NoProfile -NoExit -Command "Invoke-Expression '. ''%CMDER_ROOT%/vendor/profile.ps1'''"
+
 save_terminal=cp %TERMINAL_SETTINGS% $1
 load_terminal=cp $1 %TERMINAL_SETTINGS%  
 
@@ -24,13 +22,16 @@ load_terminal=cp $1 %TERMINAL_SETTINGS%
 
 gs=git status
 ga=git add $*
-gc=git commit -m $1
-gps=git push
-gpl=git pull
-gl=git log --oneline --graph --decorate --all
-gch=git checkout $1
+gc=git commit $*
+gps=git push $*
+gpl=git pull $*
+gl=git log --oneline --decorate $*
+gch=git checkout $*
 gb=git branch $*
 gd=git diff $*
+gf=git fetch $*
+grs=git restore $*
+gr=git rebase $*
 
 reload=call %CMDER_ROOT%\vendor\init.bat
 
@@ -39,3 +40,9 @@ cl=clear
 exp=explorer .
 
 ports=netstat -an
+cc=claude --dangerously-skip-permissions  
+
+nrd=npm run dev
+nrp=npm run preview
+nrb=npm run build
+nrt=npm run test
